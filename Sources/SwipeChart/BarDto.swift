ac0a10f93d8610355e2117f2d979f552f4384b69
Eddie Kwon <36284765+eddiekwon@users.noreply.github.com>
2021-09-03 12:12:38 +0900

    Update BarDto.swift
    made init to barDto

--- Sources/SwipeChart/BarDto.swift
+++ Sources/SwipeChart/BarDto.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 public struct BarDto {
-    public let candle_date_time_utc: String
-    public let candle_date_time_local: String
-    public let opening_price: Double
-    public let high_price: Double
-    public let low_price: Double
-    public let trade_price: Double
-    public let timestamp: TimeInterval?
-    public let candle_acc_trade_price: Double
-    public let candle_acc_trade_volume: Double
-    public let unit: Int?
+    let candle_date_time_utc: String
+    let candle_date_time_local: String
+    
+    let opening_price: Double
+    let high_price: Double
+    let low_price: Double
+    let trade_price: Double
+    
+    let timestamp: TimeInterval?
+    let candle_acc_trade_price: Double
+    let candle_acc_trade_volume: Double
+    
+    let unit: Int?
     
     public var openVal: CGFloat {
         CGFloat(opening_price)
@@ -31,5 +34,22 @@ public struct BarDto {
     public var lowVal: CGFloat {
         CGFloat(low_price)
     }
+     
+    public init(candle_date_time_utc: String, candle_date_time_local: String,
+         opening_price: Double, high_price: Double, low_price: Double, trade_price: Double,
+         timestamp: TimeInterval?, candle_acc_trade_price: Double, candle_acc_trade_volume: Double,
+         unit: Int?) {
+        
+        self.candle_date_time_utc    = candle_date_time_utc
+        self.candle_date_time_local  = candle_date_time_local
+        self.opening_price           = opening_price
+        self.high_price              = high_price
+        self.low_price               = low_price
+        self.trade_price             = trade_price
+        self.timestamp               = timestamp
+        self.candle_acc_trade_price  = candle_acc_trade_price
+        self.candle_acc_trade_volume = candle_acc_trade_volume
+        self.unit                    = unit
+         
+    }
 }
-
